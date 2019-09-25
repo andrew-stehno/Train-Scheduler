912ba7247827aa617b439a3c64bc0c710d8ca217
andrew stehno <andrew_stehno@yahoo.com>
2019-09-25 15:30:10 -0600

    created some basic bootstrap componets in html

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCGpN3WVXZki6csqb5r623irkpchpGjs2w",
+    authDomain: "train-time-bf7b5.firebaseapp.com",
+    databaseURL: "https://train-time-bf7b5.firebaseio.com",
+    projectId: "train-time-bf7b5",
+    storageBucket: "",
+    messagingSenderId: "1025189797521",
+    appId: "1:1025189797521:web:6203b58d192e1742ed6930"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
