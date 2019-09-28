8ceb9d1b855bb2e5438560cebcee695751bde635
andrew stehno <andrew_stehno@yahoo.com>
2019-09-27 21:21:20 -0600

    created moment.js functions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,6 +13,7 @@ firebase.initializeApp(firebaseConfig);
 
 // Globally scoped variables:
 let database = firebase.database();
+let now = moment();
 // Initiall values:
 let trainName = "";
 let destination = "";
@@ -31,13 +32,15 @@ $("#submit-btn").on("click", function (event) {
   firstTrainTime = $("#first-train-time").val().trim();
   frequency = $("#frequency").val().trim();
 
-  // Empty form values:
-  $("#train-name").val("");
-  $("#destination").val("");
-  $("#first-train-time").val("");
-  $("#frequency").val("");
-
   // Mathmatical calculations:
+  
+  
+  // compare first train time to now in 24 hour time
+  // is new train time > or < now
+  // if new train time is > now, next arrival = difference of two
+  // if new train time is < now, what is the %(frequency) of the difference?
+  // next arrival would = % + frequency
+
 
   //Push to Firebase:
   database.ref().push({
@@ -49,6 +52,12 @@ $("#submit-btn").on("click", function (event) {
     NextArrival: nextArr,
     MinutesAway: minAway
   });
+
+  // Empty form values:
+  $("#train-name").val("");
+  $("#destination").val("");
+  $("#first-train-time").val("");
+  $("#frequency").val("cd");
 });
 
 // Firebase listener:
@@ -61,6 +70,28 @@ database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val().FirstTrain);
   console.log(childSnapshot.val().Frequency);
 
+  // Calculations using moment.js for "next arrival" and "minutes away"
+  let timeArr = childSnapshot.val().FirstTrain.split(":");
+  console.log(timeArr);
+  let trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+  console.log(trainTime);
+  let maxMoment = moment.max(moment(),trainTime);
+  console.log(maxMoment);
+  let tMin;
+  let tArrival;
+
+  if (maxMoment === trainTime) {
+    tArrival = trainTime.format("hh:mm A");
+    tMin = trainTime.diff(moment(), "minutes");
+  } else {
+    let difTime = moment().diff(trainTime, "minutes");
+    let tRemain = difTime % childSnapshot.val().Frequency;
+    tMin = childSnapshot.val().Frequency - tRemain;
+    tArrival = moment().add(tMin, "m").format("hh:mm A");
+  }
+  console.log(tArrival);
+  console.log(tMin);
+  
   // Post data to DOM:
   $('#train-name-target').append(childSnapshot.val().Train);
   $('#destination-target').append(childSnapshot.val().Destination);
